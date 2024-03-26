c905c038b6a095d124a63706d19378190e717cac
jolson <jolson@adobe.com>
2024-03-25 21:08:46 -0600

    add requirements.txt and update app.py imports

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from collections import deque
 import cv2 as cv
 import numpy as np
 import mediapipe as mp
+import mediapipe.python.solutions.hands as mp_hands
 
 from utils import CvFpsCalc
 from model import KeyPointClassifier
@@ -58,10 +59,9 @@ def main():
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, cap_height)
 
     # Model load #############################################################
-    mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(
         static_image_mode=use_static_image_mode,
-        max_num_hands=1,
+        max_num_hands=2,
         min_detection_confidence=min_detection_confidence,
         min_tracking_confidence=min_tracking_confidence,
     )
